cd466fc971429bd2897c676fa1be7fd9c1f55c24
Tobias Liaudat <tobiasliaudat@gmail.com>
2020-10-01 17:50:04 +0200

    Added management of requirements in requirements.txt

--- setup.py
+++ setup.py
@@ -16,6 +16,9 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open('requirements.txt') as open_file:
+    install_requires = open_file.read()
+
 setup(
     name=__name__,
     author=release_info['__author__'],
@@ -23,7 +26,7 @@ setup(
     version=release_info['__version__'],
     url=release_info['__url__'],
     packages=find_packages(),
-    install_requires=release_info['__requires__'],
+    install_requires=install_requires,
     license=release_info['__license__'],
     description=release_info['__about__'],
     long_description=long_description,
