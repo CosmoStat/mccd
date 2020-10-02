1337cb8e380b82590158d5fdd474056eaeca9270
Tobias Liaudat <tobiasliaudat@gmail.com>
2020-10-02 17:48:36 +0200

    Reformat init

--- example/__init__.py
+++ example/__init__.py
@@ -6,6 +6,7 @@ Provide some basic description of the example module.
 
 """
 
-__all__ = ['hello', 'math']  # List of submodules
-
 from . import *
+
+__all__ = ['hello', 'math']  # List of submodules
+__all__ += ['auxiliary_fun_test']
