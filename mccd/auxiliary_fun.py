96b74301e02d1ebde7515b65f5f75bd8c53b8258
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-06-16 10:55:16 +0200

    Cleaning code

--- mccd/auxiliary_fun.py
+++ mccd/auxiliary_fun.py
@@ -650,7 +650,7 @@ def mccd_validation(mccd_model_path, testcat, apply_degradation=True,
                 np.concatenate(PSF_loc_list, axis=0))
         else:
 
-            print('For the moment we are cancelling the global_pol_interp')
+            
             global_pol_interp=False
 
             if global_pol_interp:
